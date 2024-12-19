ced7cfe35e6e78248ae36a1a828acb8746ae8dc9
Álvaro <alvarobotdc@gmail.com>
2024-12-19 22:24:16 +0100

    MIxed show and get_content functions

--- app.py
+++ app.py
@@ -7,38 +7,35 @@ from dotenv import load_dotenv
 from mail import send
 from discord import message
 
-def get_content(driver, id):
-    # Encontrar elemento del correo
-    xpath = f'//*[@id="{id}"]/td[2]/span[3]/a'
-    element = driver.find_element(By.XPATH, xpath)
-
-    # Abrir el correo en una nueva pestaña
-    link = element.get_attribute("href")
-    driver.execute_script(f"window.open('{link}', '_blank');")
-    driver.switch_to.window(driver.window_handles[-1])
-
-    # Asunto
-    asunto_elemento = driver.find_element(By.XPATH, '//*[@id="message-header"]/h2')
-    asunto = asunto_elemento.text
-
-    # Cuerpo
-    cuerpo_elemento = driver.find_element(By.XPATH, '//*[@id="message-htmlpart1"]/div')
-    cuerpo = cuerpo_elemento.text
-
-    # Enviar correo a cuenta personal
-    send(asunto, cuerpo)
-
-    # Enviar mensaje por Discord
-    message(asunto, cuerpo)
-
-    # Cerrar pestaña
-    driver.close()
-    driver.switch_to.window(driver.window_handles[0])
-
-def show(tr_elements, driver):
+def get_content(driver, tr_elements):
     for tr in tr_elements:
-        id = tr.get_attribute("id")
-        get_content(driver, id)
+        id = tr.get_attribute('id')
+        # Encontrar elemento del correo
+        xpath = f'//*[@id="{id}"]/td[2]/span[3]/a'
+        element = driver.find_element(By.XPATH, xpath)
+
+        # Abrir el correo en una nueva pestaña
+        link = element.get_attribute("href")
+        driver.execute_script(f"window.open('{link}', '_blank');")
+        driver.switch_to.window(driver.window_handles[-1])
+
+        # Asunto
+        asunto_elemento = driver.find_element(By.XPATH, '//*[@id="message-header"]/h2')
+        asunto = asunto_elemento.text
+
+        # Cuerpo
+        cuerpo_elemento = driver.find_element(By.XPATH, '//*[@id="message-htmlpart1"]/div')
+        cuerpo = cuerpo_elemento.text
+
+        # Enviar correo a cuenta personal
+        send(asunto, cuerpo)
+
+        # Enviar mensaje por Discord
+        message(asunto, cuerpo)
+
+        # Cerrar pestaña
+        driver.close()
+        driver.switch_to.window(driver.window_handles[0])
 
 def scrape():
     # Cargar variables de entorno
@@ -71,7 +68,7 @@ def scrape():
     tr_elements = driver.find_elements(By.CSS_SELECTOR, 'tr.unread')
 
     if tr_elements:
-        show(tr_elements, driver)
+        get_content(driver, tr_elements)
     else:
         print('Ningún mensaje detectado.')
 
