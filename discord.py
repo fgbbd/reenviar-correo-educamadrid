b1067d7c15575cfbc567fd69dd35095b4189c24d
Álvaro <alvarobotdc@gmail.com>
2024-12-24 23:09:20 +0100

    fixed typo

--- discord.py
+++ discord.py
@@ -5,7 +5,7 @@ def message(asunto, cuerpo, link):
     token = os.getenv('TOKEN')
 
     # Asignar URL, Autorización y Contenido del mensaje
-    id = 1133821801051467827 # Reemplza con la ID del canal MD
+    id = 1133821801051467827 # Reemplaza con la ID del canal MD
     url = f'https://discord.com/api/v10/channels/{id}/messages'
     headers = {'Authorization': token}
     json = {'content': f'# [{asunto}](<{link}>)\n{cuerpo}'}
