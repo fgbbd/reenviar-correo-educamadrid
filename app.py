4b7c14995fde8a2f03faffc01b5af638d2a366d8
Álvaro V. <165289321+fgbbd@users.noreply.github.com>
2025-02-28 12:41:50 +0100

    Set sleep to 30

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def main():
     driver = webdriver.Chrome(options=options)
     driver.get("https://correoweb.educa.madrid.org")
 
-    time.sleep(5)
+    time.sleep(30)
     
     # Definir pausa hasta que los elementos existan
     wait = WebDriverWait(driver, 10) 
